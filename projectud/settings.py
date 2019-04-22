d1e680138ff817e86c9ae4c1877fee9a76c6e90d
Kadir Çeven <kadrocuk@gmail.com>
2019-04-22 23:07:57 +0300

    about database

--- projectud/settings.py
+++ projectud/settings.py
@@ -132,11 +132,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# ie if Heroku server
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+
 
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
