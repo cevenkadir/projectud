c4c5631928e2e31de73b5dacbee7ead02b3aeb09
Kadir Çeven <kadrocuk@gmail.com>
2019-04-22 20:24:15 +0300

    updating gitignore

--- projectud/settings.py
+++ projectud/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 from dotenv import load_dotenv
 
@@ -132,3 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
