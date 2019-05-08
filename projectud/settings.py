5594828e8003c12a4b89bf41eade63eb7db83ccc
Kadir Çeven <kadrocuk@gmail.com>
2019-05-09 00:47:25 +0300

    Configuring.

--- projectud/settings.py
+++ projectud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "1xg)tz&v_8m^!f6)p_w^ucuoy(&c9jtdtu$fdxi9_uhg(bt#dr"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'projectud.herokuapp.com']
 
 
 # Application definition
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'projectud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
