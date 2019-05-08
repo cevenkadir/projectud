c5d2bc4deadbb2443ac01d532426b61ac823b29c
Kadir Çeven <kadrocuk@gmail.com>
2019-05-09 00:51:40 +0300

    Updating settings.py

--- projectud/settings.py
+++ projectud/settings.py
@@ -140,3 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
