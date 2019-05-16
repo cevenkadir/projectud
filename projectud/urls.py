1574e1bbfb660d5a7659e2e8a257b01e3bd12610
Kadir Çeven <kadrocuk@gmail.com>
2019-05-16 14:52:26 +0300

    On media url

--- projectud/urls.py
+++ projectud/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.views.static import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +29,7 @@ urlpatterns = [
     
 ]
 urlpatterns += staticfiles_urlpatterns()
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
-    from django.conf.urls.static import static
+    
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
